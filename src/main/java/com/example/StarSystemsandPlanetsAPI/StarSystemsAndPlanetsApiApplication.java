b5f56711ff45bc8859012f9be8ff4e24036c3de1
Cem Gurhan <cemg@live.co.uk>
2022-05-18 20:01:18 +0100

    first commit

---
+++ src/main/java/com/example/StarSystemsandPlanetsAPI/StarSystemsAndPlanetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.StarSystemsandPlanetsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarSystemsAndPlanetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarSystemsAndPlanetsApiApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/StarSystemsandPlanetsAPI/StarSystemsAndPlanetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StarSystemsandPlanetsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarSystemsAndPlanetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

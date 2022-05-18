9c051410ff4454f114e944ed356edb63b564f4ad
Cem Gurhan <cemg@live.co.uk>
2022-05-18 21:50:01 +0100

    experimented and managed to initialize postgres database tables after changing application properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO stars (constellation,name,solar_mass,type) VALUES ("Cygnus","Kepler-16A",0.6897,"binary");
+INSERT INTO stars (constellation,name,solar_mass,type) VALUES ('Cygnus','Kepler-16A',0.6897,'binary');

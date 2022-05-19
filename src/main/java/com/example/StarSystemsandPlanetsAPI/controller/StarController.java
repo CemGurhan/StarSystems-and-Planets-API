84d119979e7b69630d8fe3e8765ed7a7ee435fea
Cem Gurhan <cemg@live.co.uk>
2022-05-19 08:00:44 +0100

    Added getMapping for star. Tested both endpoints for planets and stars using postman

--- src/main/java/com/example/StarSystemsandPlanetsAPI/controller/StarController.java
+++ src/main/java/com/example/StarSystemsandPlanetsAPI/controller/StarController.java
@@ -1,7 +1,30 @@
 package com.example.StarSystemsandPlanetsAPI.controller;
 
+import com.example.StarSystemsandPlanetsAPI.model.Star;
+import com.example.StarSystemsandPlanetsAPI.repository.StarRepository;
+import com.example.StarSystemsandPlanetsAPI.service.StarService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StarController {
+
+    private StarService starService;
+
+    public StarController(StarService starService){
+
+        this.starService = starService;
+    }
+
+    @GetMapping("/stars")
+    public List<Star> findAll(){
+        return starService.findAll();
+    }
+
+
+
+
+
 }
--- src/main/java/com/example/StarSystemsandPlanetsAPI/service/StarService.java
+++ src/main/java/com/example/StarSystemsandPlanetsAPI/service/StarService.java
@@ -1,7 +1,23 @@
 package com.example.StarSystemsandPlanetsAPI.service;
 
+import com.example.StarSystemsandPlanetsAPI.model.Star;
+import com.example.StarSystemsandPlanetsAPI.repository.StarRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StarService {
+
+    private StarRepository starRepository;
+
+    public StarService(StarRepository starRepository){
+        this.starRepository = starRepository;
+
+    }
+
+    public List<Star> findAll(){
+        return starRepository.findAll();
+    }
+
 }
